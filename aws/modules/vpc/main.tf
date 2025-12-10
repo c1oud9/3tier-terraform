f73c6590d6bbe9435452847536ad4429d2f8872d
c1oud9 <reyne9@naver.com>
2025-12-10 23:57:34 +0000

    bInitial commit

--- aws/modules/vpc/main.tf
+++ aws/modules/vpc/main.tf
@@ -33,11 +33,6 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-resource "aws_internet_gateway_attachment" "main" {
-  internet_gateway_id = aws_internet_gateway.main.id
-  vpc_id              = aws_vpc.main.id
-}
-
 
 
 
